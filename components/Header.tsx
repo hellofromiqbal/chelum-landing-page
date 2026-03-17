d4654fa08d4d6e99ca709ac2898e2679f6a5debe
Muhammad Iqbal <hellofromiqbal@gmail.com>
2026-03-17 10:43:12 +0700

    Fixed header height

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-[#05132B] backdrop-blur-sm border-b border-amber-900/20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-20">
+        <div className="flex justify-between items-center h-14 md:h-20">
           {/* Logo */}
           <Link
             href={"#home"}
